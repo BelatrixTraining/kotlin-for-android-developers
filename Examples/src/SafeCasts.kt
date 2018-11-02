49a6ba6003b51d6bcf902d9455aee9caf36a5ce7
Alonso <alonsopf1@gmail.com>
2018-11-02 17:53:05 -0500

    Add safe cast examples

---
+++ Examples/src/SafeCasts.kt
@@ -0,0 +1,27 @@
+data class Developer(var name: String)
+data class QA(var name: String)
+
+fun main(args: Array<String>) {
+    val dev = Developer("Gunnaringumu Diminson, Yugunothwer Misinjawer")
+    val qa = QA("Ckipther Alewphawer Yacunah, Zumac Yaxche")
+
+    soYouWannaGetClassCastException(dev)
+    soYouWannaGetClassCastException(qa)
+
+    println("*******************************************************")
+
+    soYouWannaBeTheMasterOfClassCasting2018ForeverSingle(dev)
+    soYouWannaBeTheMasterOfClassCasting2018ForeverSingle(qa)
+}
+
+fun soYouWannaGetClassCastException(anyone: Any) {
+    try {
+        println((anyone as Developer).name)
+    } catch (exception: ClassCastException) {
+        println("oh damn it! :(")
+    }
+}
+
+fun soYouWannaBeTheMasterOfClassCasting2018ForeverSingle(anyone: Any) {
+    println((anyone as? Developer)?.name ?: "nothing to show, you are not a developer :)")
+}
